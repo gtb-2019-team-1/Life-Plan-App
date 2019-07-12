56f31c30f07bc8c42e9d550738fba622657e3b1e
Y.Fukunaga <yuma-fukunaga@gmo.jp>
2019-07-12 18:59:38 +0900

    update: 決め打ちデータに更新することはできたが描画されない

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React from 'react';
 import './App.css';
 import {LineChart, Line, CartesianGrid, ResponsiveContainer, XAxis, YAxis, Tooltip} from 'recharts';
 
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    let init_data = [];
+    for(let age = 0; age <= 100; age++){
+        init_data.push({age:(age), annual_income:0});
+    }
+    
     this.state = {
-        age:'',
-        annual_income:'',
-        annual_spending:'',
-        asset:'',
-        total_asset:'',
-        data: [{time: '2019-07-04', annual_income: 400}, {time: '2019-07-05', annual_income: 300}, {time: '2019-07-06', annual_income: 100}, {time: '2019-07-07', annual_income: 400}]
+        starting_age:20,
+        annual_income:0,
+        annual_spending:0,
+        asset:0,
+        total_asset:0,
+        data: init_data,
+        got_data: [{age: 20, annual_income: 200}, {age: 21, annual_income: 200}, {age: 22, annual_income: 300}]
     };
-    this.handleChange = this.handleChange.bind(this);
+    this.annualIncomeHandleChange = this.annualIncomeHandleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getData = this.getData.bind(this);
   }
 
   componentDidMount() {
@@ -24,36 +30,55 @@ export default class App extends React.Component {
   componentWillUnmount() {
   }
 
-  handleChange(event) {
+  annualIncomeHandleChange(event) {
     this.setState({annual_income: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A time was submitted: ' + this.state.annual_income);
-    //this.state.data.push({time: 'new', annual_income: this.state.annual_income}); // array破壊している。以下の方法またはstate.dataをオブジェクトコピーとして更新
     this.setState((state) => {
-      return {data: state.data.concat({time: 'new', annual_income: parseInt(state.annual_income)})}
+      return {
+          data: state.data.concat({age: 120, annual_income: parseInt(state.annual_income)})
+      }
     });
     event.preventDefault();
+  }
+
+  getData(event){
+      let copied_data = this.state.data;
+      let copied_got_data = this.state.got_data;
+      for(let i=0; i<copied_got_data.length; i++){
+           for(let j=0; j<copied_data.length; j++){
+               if(copied_data[j].age === copied_got_data[i].age) copied_data[j].annual_income = parseInt(copied_got_data[i].annual_income);
+           }
+      }
+      this.setState((state) => {
+          return {
+              data: copied_data
+          }
+      });
+      event.preventDefault();
   }
 
   render() {
     return (
       <div>
+        {/*
         <form onSubmit={this.handleSubmit}>
           <label>
             annual_income:
-            <input name="annual_income" value={this.state.annual_income} type="text" onChange={this.handleChange}/>
+            <input name="annual_income" value={this.state.annual_income} type="text" onChange={this.annualIncomeHandleChange}/>
           </label>
           <input type="submit" value="Submit" />
         </form>
+        */}
+        <a onClick={this.getData} data={this.state.data} got_data={this.state.got_data}>げっと！</a>
         {/* {console.log(this.state.data)} */}
-        <LineChart width={600} height={300} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-          <Line type="monotone" dataKey="annual_income" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-          <XAxis dataKey="time" />
+        {console.log(this.state.data)}
+        <LineChart width={1000} height={500} data={this.state.data}>
+          <XAxis dataKey="age" />
           <YAxis />
-          <Tooltip />
+          <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+          <Line type="monotone" dataKey="annual_income" stroke="#8884d8" />
         </LineChart>
       </div>
     );

e82b22f55b3f00191fa1e50f470b76892d343d35
Y.Fukunaga <yuma-fukunaga@gmo.jp>
2019-07-12 11:20:52 +0900

    update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,12 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-        money:'',
-        data: [{time: '2019-07-04', money: 400}, {time: '2019-07-05', money: 300}, {time: '2019-07-06', money: 100}, {time: '2019-07-07', money: 400}]
+        age:'',
+        annual_income:'',
+        annual_spending:'',
+        asset:'',
+        total_asset:'',
+        data: [{time: '2019-07-04', annual_income: 400}, {time: '2019-07-05', annual_income: 300}, {time: '2019-07-06', annual_income: 100}, {time: '2019-07-07', annual_income: 400}]
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,14 +25,14 @@ export default class App extends React.Component {
   }
 
   handleChange(event) {
-    this.setState({money: event.target.value});
+    this.setState({annual_income: event.target.value});
   }
 
   handleSubmit(event) {
-    alert('A time was submitted: ' + this.state.money);
-    //this.state.data.push({time: 'new', money: this.state.money}); // array破壊している。以下の方法またはstate.dataをオブジェクトコピーとして更新
+    alert('A time was submitted: ' + this.state.annual_income);
+    //this.state.data.push({time: 'new', annual_income: this.state.annual_income}); // array破壊している。以下の方法またはstate.dataをオブジェクトコピーとして更新
     this.setState((state) => {
-      return {data: state.data.concat({time: 'new', money: parseInt(state.money)})}
+      return {data: state.data.concat({time: 'new', annual_income: parseInt(state.annual_income)})}
     });
     event.preventDefault();
   }
@@ -38,14 +42,14 @@ export default class App extends React.Component {
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Money:
-            <input name="money" value={this.state.money} type="text" onChange={this.handleChange}/>
+            annual_income:
+            <input name="annual_income" value={this.state.annual_income} type="text" onChange={this.handleChange}/>
           </label>
           <input type="submit" value="Submit" />
         </form>
         {/* {console.log(this.state.data)} */}
         <LineChart width={600} height={300} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-          <Line type="monotone" dataKey="money" stroke="#8884d8" />
+          <Line type="monotone" dataKey="annual_income" stroke="#8884d8" />
           <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
           <XAxis dataKey="time" />
           <YAxis />
